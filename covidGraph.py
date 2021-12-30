cafc6f91c7dd95b5ca447b4b991a32a6eef770c2
ThunderCloud228 <83110279+ThunderCloud228@users.noreply.github.com>
2021-12-30 11:46:53 +0530

    Update covidGraph.py

--- covidGraph.py
+++ covidGraph.py
@@ -2,6 +2,6 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv("covidData.csv")
-fig = px.scatter(df, x="date", y="cases", size="cases", color="country", size_max=50)
+fig = px.scatter(df, x="date", y="cases", color="country")
 fig.show()
 
